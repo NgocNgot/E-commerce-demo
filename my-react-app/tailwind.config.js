2b631d9a1d05a8ee6b25a6a3232e4a6345405161
Bich Ngoc <138796600+NgocNgot@users.noreply.github.com>
2025-03-21 17:57:20 +0700

    Delete my-react-app directory

--- my-react-app/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Kiểm tra lại đường dẫn này
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
